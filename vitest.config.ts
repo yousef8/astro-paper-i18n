106d2f40b30d866ee37e4a5f2e3f109ca061b5cc
Yousef Ghonim <37050129+yousef8@users.noreply.github.com>
2025-02-21 21:00:36 +0200

    test: setup vitest with coverage-v8

---
+++ vitest.config.ts
@@ -0,0 +1,12 @@
+/// <reference types="vitest" />
+import { getViteConfig } from 'astro/config';
+import { coverageConfigDefaults } from 'vitest/config'
+
+export default getViteConfig({
+	test: {
+    coverage: {
+      include:['src/**/*'],
+      exclude: ['src/assets', 'src/content', 'src/**/types.ts', 'src/i18n/locales', ...coverageConfigDefaults.exclude]
+    }
+	},
+});
