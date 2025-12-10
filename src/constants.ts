c3325df9720b0387d1c1a2d68c3c0d05dbaac90f
Yousef Ghonim <37050129+yousef8@users.noreply.github.com>
2025-12-11 00:36:40 +0200

    feat: sync AstroPaper v5.3.0 (#19)

--- src/constants.ts
+++ src/constants.ts
@@ -1,16 +1,24 @@
-import IconMail from "@/assets/icons/IconMail.svg";
-import IconGitHub from "@/assets/icons/IconGitHub.svg";
 import IconBrandX from "@/assets/icons/IconBrandX.svg";
-import IconLinkedin from "@/assets/icons/IconLinkedin.svg";
-import IconWhatsapp from "@/assets/icons/IconWhatsapp.svg";
 import IconFacebook from "@/assets/icons/IconFacebook.svg";
-import IconTelegram from "@/assets/icons/IconTelegram.svg";
+import IconGitHub from "@/assets/icons/IconGitHub.svg";
+import IconLinkedin from "@/assets/icons/IconLinkedin.svg";
+import IconMail from "@/assets/icons/IconMail.svg";
 import IconPinterest from "@/assets/icons/IconPinterest.svg";
+import IconTelegram from "@/assets/icons/IconTelegram.svg";
+import IconWhatsapp from "@/assets/icons/IconWhatsapp.svg";
 import { translateFor } from "@/i18n/utils";
+import type { Props } from "astro";
 
 type Translator = ReturnType<typeof translateFor>;
 
-export const SOCIALS = [
+interface Social {
+  name: string;
+  href: string;
+  linkTitle: (t: Translator) => string;
+  icon: (_props: Props) => Element;
+}
+
+export const SOCIALS: Social[] = [
   {
     name: "Github",
     href: "https://github.com/yousef8/astro-paper-i18n",
@@ -37,7 +45,7 @@ export const SOCIALS = [
   },
 ] as const;
 
-export const SHARE_LINKS = [
+export const SHARE_LINKS: Social[] = [
   {
     name: "WhatsApp",
     href: "https://wa.me/?text=",
